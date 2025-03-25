8a8c01f459c352c177785946e5bfc4e3165df2e7
kpbaks <kristoffer.pbs@tuta.io>
2025-03-25 19:14:37 +0100

    get ros2_rust to compile with flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/develop";
     nixpkgs.follows = "nix-ros-overlay/nixpkgs";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
   outputs =
     {
       self,
       nix-ros-overlay,
       nixpkgs,
+      nixpkgs-unstable,
     }:
     let
       # List of ROS2 distributions can be found here: https://docs.ros.org/en/jazzy/Releases.html
@@ -20,25 +22,41 @@
           inherit system;
           overlays = [ nix-ros-overlay.overlays.default ];
         };
+        pkgs-unstable = import nixpkgs-unstable { inherit system; };
       in
 
       {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            colcon
-            rustc
-            cargo
-            (
-              with pkgs.rosPackages.${ros2-distribution};
-              buildEnv {
-                paths = [
-                  ros-core
-                  turtlesim
-                  # ... other ROS packages
-                ];
-              }
-            )
-          ];
+        devShells.default = pkgs.mkShell rec {
+          nativeBuildInputs = buildInputs;
+          buildInputs =
+            (with pkgs-unstable; [
+              rustc
+              rustfmt
+              rust-analyzer
+              cargo
+            ])
+            ++ (with pkgs; [
+              libclang.lib
+              clang
+              clang-tools
+              gcc
+              stdenv
+              pkg-config
+              cmake
+              binutils
+              colcon
+              (
+                with pkgs.rosPackages.${ros2-distribution};
+                buildEnv {
+                  paths = [
+                    ros-core
+                    turtlesim
+                    # ... other ROS packages
+                  ];
+                }
+              )
+            ]);
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
         };
       }
     );
